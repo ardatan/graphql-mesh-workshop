52cd965ef176d67decdf96f72bcf2be8a92dba4b
Arda TANRIKULU <ardatanrikulu@gmail.com>
2020-06-10 17:30:18 +0300

    Connect two APIs each other

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,30 @@ import { GeoDBAPI } from './geodb/dataSource';
 import { openBreweryTypeDefs } from './openbrewery/typeDefs';
 import { openBreweryResolvers } from './openbrewery/resolvers';
 import { OpenBreweryAPI } from './openbrewery/dataSource';
+import { GraphQLResolveInfo } from 'graphql';
+
+const linkTypeDefs = /* GraphQL */`
+    extend type City {
+        breweries: [Brewery]
+    }
+`;
+
+const linkResolvers = {
+    City: {
+        breweries: (root: any, args: any, context: any, info: GraphQLResolveInfo) => context.dataSources.openBreweryAPI.findBreweries({ by_city: root.name }),
+    }
+};
 
 const apolloServer = new ApolloServer({
     typeDefs: [
         geoDBTypeDefs,
         openBreweryTypeDefs,
+        linkTypeDefs
     ],
     resolvers: [
         geoDBResolvers,
         openBreweryResolvers,
+        linkResolvers,
     ],
     dataSources: () => ({
         geoDBAPI: new GeoDBAPI(),
